7aecc709508b2e8d0846871be6ccc5f1564a99c1
Craig D. Wetzelberger <cdwetzelberger@bamboohealth.com>
2024-08-21 14:16:38 -0400

    update

--- example/index.js
+++ example/index.js
@@ -1,2 +1,17 @@
 
 console.log('!!!!!!!!----------------from index.js')
+
+function component() {
+
+  function Test() {
+    return 'hello from Test'
+  }
+
+  return {
+    "string": "hello from component",
+    "Test": Test
+  }
+}
+
+
+export default component;
